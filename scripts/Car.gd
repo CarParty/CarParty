0a2a4957af99ec0d1e22d9b890e00f504a41784d
Linus Heck <linus.heck@rwth-aachen.de>
2021-11-19 16:21:11 +0100

    pathfollow

--- scripts/Car.gd
+++ scripts/Car.gd
@@ -3,7 +3,7 @@ extends VehicleBody
 ############################################################
 # behaviour values
 
-export var MAX_ENGINE_FORCE = 400.0
+export var MAX_ENGINE_FORCE = 150.0
 export var MAX_BRAKE_FORCE = 5.0
 export var MAX_STEER_ANGLE = 30
 
@@ -22,11 +22,15 @@ export var throttle_mult = 1.0
 export var joy_brake = JOY_ANALOG_L2
 export var brake_mult = 1.0
 
+export var follow_length = 1.5
+
 onready var path = get_parent().get_node("Path")
 onready var path_follow = PathFollow.new()
 
 func _ready():
 	path.add_child(path_follow)
+	path_follow.offset = 0.0
+	path_follow.loop = true
 
 func _physics_process(delta):
 	# overrules for keyboard
@@ -40,18 +44,26 @@ func _physics_process(delta):
 	engine_force = throttle_val * MAX_ENGINE_FORCE
 	brake = brake_val * MAX_BRAKE_FORCE
 	
+	
 	# compute distance between pathfollow and this
-	var projected_translation = Vector2(self.translation.x, self.translation.z)
+	var projected_translation = Vector2(self.get_global_transform().origin.x, self.get_global_transform().origin.z)
 	#print(projected_translation)
-	var path_follow_translation = Vector2(path_follow.translation.x, path_follow.translation.z)
+	var path_follow_translation = Vector2(path_follow.get_global_transform().origin.x, path_follow.get_global_transform().origin.z)
 	#print(path_follow_translation)
+	
+	# move ahead the pathfollow
+	while (path_follow_translation - projected_translation).length() < follow_length:
+		path_follow.offset += 0.05
+		path_follow_translation = Vector2(path_follow.get_global_transform().origin.x, path_follow.get_global_transform().origin.z)
+	
 	var own_normal = Vector2(self.transform.basis.z.x, self.transform.basis.z.z)
-	print("own normal", own_normal)
+	
+	#print("own normal", own_normal)
 	var delta_vec = (path_follow_translation - projected_translation).normalized()
-	print("path follow - own", delta_vec)
+	#print("path follow - own", delta_vec)
 	var angle = -own_normal.angle_to(delta_vec)
-	print("angle", angle)
-	var steer_target = angle
+	#print("angle", angle)
+	steer_target = angle
 	if (steer_target < steer_angle):
 		steer_angle -= steer_speed * delta
 		if (steer_target > steer_angle):
@@ -60,5 +72,6 @@ func _physics_process(delta):
 		steer_angle += steer_speed * delta
 		if (steer_target < steer_angle):
 			steer_angle = steer_target
-	
+	steer_angle = min(MAX_STEER_ANGLE, steer_angle)
+	steer_angle = max(-MAX_STEER_ANGLE, steer_angle)
 	steering = steer_angle
