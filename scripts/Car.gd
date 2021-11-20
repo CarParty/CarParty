9b9cdfba7de99a7151f27b34125ef202e1f3882e
Linus Heck <linus.heck@rwth-aachen.de>
2021-11-20 10:04:57 +0100

    make track bigger and follow a bit better

--- scripts/Car.gd
+++ scripts/Car.gd
@@ -27,23 +27,30 @@ export var follow_length = 1.5
 onready var path = get_parent().get_node("Path")
 onready var path_follow = PathFollow.new()
 
+onready var debug_sphere = load("scenes/DebugSphere.tscn").instance()
+
 func _ready():
 	path.add_child(path_follow)
 	path_follow.offset = 0.0
 	path_follow.loop = true
+	path_follow.add_child(debug_sphere)
 
 func _physics_process(delta):
-	# overrules for keyboard
 	var throttle_val = 0.0
 	var brake_val = 0.0
+	var reverse_val = 0.0
 	if Input.is_action_pressed("ui_up"):
 		throttle_val = 1.0
 	if Input.is_action_pressed("ui_down"):
 		brake_val = 1.0
+		if (self.transform.basis * self.linear_velocity).z < 0.5:
+			brake_val = 0.0
+			throttle_val = -0.5
+		else:
+			brake_val = 1.0
 		
 	engine_force = throttle_val * MAX_ENGINE_FORCE
 	brake = brake_val * MAX_BRAKE_FORCE
-	
 	
 	# compute distance between pathfollow and this
 	var projected_translation = Vector2(self.get_global_transform().origin.x, self.get_global_transform().origin.z)
@@ -51,10 +58,13 @@ func _physics_process(delta):
 	var path_follow_translation = Vector2(path_follow.get_global_transform().origin.x, path_follow.get_global_transform().origin.z)
 	#print(path_follow_translation)
 	
+	var closest_offset = path.curve.get_closest_offset(path.to_local(self.get_global_transform().origin))
+	path_follow.offset = closest_offset + follow_length
+	
 	# move ahead the pathfollow
-	while (path_follow_translation - projected_translation).length() < follow_length:
-		path_follow.offset += 0.05
-		path_follow_translation = Vector2(path_follow.get_global_transform().origin.x, path_follow.get_global_transform().origin.z)
+	#while (path_follow_translation - projected_translation).length() < follow_length:
+	#	path_follow.offset += 0.05
+	#	path_follow_translation = Vector2(path_follow.get_global_transform().origin.x, path_follow.get_global_transform().origin.z)
 	
 	var own_normal = Vector2(self.transform.basis.z.x, self.transform.basis.z.z)
 	
@@ -62,13 +72,16 @@ func _physics_process(delta):
 	var delta_vec = (path_follow_translation - projected_translation).normalized()
 	#print("path follow - own", delta_vec)
 	var angle = -own_normal.angle_to(delta_vec)
+	
+	
+	
 	#print("angle", angle)
 	steer_target = angle
-	if (steer_target < steer_angle):
+	if steer_target < steer_angle and steer_angle > -MAX_STEER_ANGLE:
 		steer_angle -= steer_speed * delta
-		if (steer_target > steer_angle):
+		if steer_target > steer_angle:
 			steer_angle = steer_target
-	elif (steer_target > steer_angle):
+	elif steer_target > steer_angle and steer_angle < MAX_STEER_ANGLE:
 		steer_angle += steer_speed * delta
 		if (steer_target < steer_angle):
 			steer_angle = steer_target
