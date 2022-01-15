e6e5754df25083669ff9f617f33fef2f876448cf
Marvin Vogt <marvin.vogt@rwth-aachen.de>
2022-01-15 22:15:39 +0100

    joinPhase: add rough version of animated logo

--- CarPartyClient/src/index.ts
+++ CarPartyClient/src/index.ts
@@ -2,6 +2,7 @@ import './car-party.component';
 import './drawing-phase/drawingPhase.component';
 import './drawing-phase/help-modal/helpModal.component';
 import './ending-phase/endingPhase.component';
+import './join-phase/animated-splash/animatedSplash.component';
 import './join-phase/download-modal/downloadModal.component';
 import './join-phase/joinPhase.component';
 import './naming-phase/namingPhase.component';
---
+++ CarPartyClient/src/join-phase/animated-splash/animatedSplash.component.ts
@@ -0,0 +1,46 @@
+import css from './animatedSplash.component.css';
+import template from './animatedSplash.component.html';
+
+const templateEl = document.createElement('template');
+templateEl.innerHTML = template;
+
+const cssContainer = document.createElement('style');
+cssContainer.textContent = css;
+
+export class AnimatedSplashComponent extends HTMLElement {
+  private shadow: ShadowRoot;
+  private root: HTMLElement | null;
+
+  static get observedAttributes(): string[] {
+    return [];
+  }
+
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'closed' });
+    this.shadow = shadow;
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'assets/styles.css';
+    this.shadow.appendChild(link);
+    shadow.appendChild(templateEl.content.cloneNode(true));
+    shadow.appendChild(cssContainer.cloneNode(true));
+
+    this.root = shadow.getElementById('root');
+  }
+
+  public attributeChangedCallback(name: string, oldValue: string, newValue: string): void { }
+
+  public connectedCallback(): void { }
+
+  public disconnectedCallback(): void { }
+}
+
+window.customElements.define('animated-splash', AnimatedSplashComponent);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'animated-splash': AnimatedSplashComponent;
+  }
+}
