db031a7935ff539c4cf8320ce36c3a2173a561f9
Marvin Vogt <marvin.vogt@rwth-aachen.de>
2022-01-30 15:46:15 +0100

    drawingPhase: include overlays in build - HOTFIX

--- CarPartyClient/src/index.ts
+++ CarPartyClient/src/index.ts
@@ -2,6 +2,8 @@ import './car-party.component';
 import './disconnect-modal/disconnectModal.component';
 import './drawing-phase/drawingPhase.component';
 import './drawing-phase/help-modal/helpModal.component';
+import './drawing-phase/instructions-overlay/instructionsOverlay.component';
+import './drawing-phase/loading-overlay/loadingOverlay.component';
 import './ending-phase/endingPhase.component';
 import './join-phase/animated-splash/animatedSplash.component';
 import './join-phase/download-modal/downloadModal.component';
